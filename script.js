00a1fe2c8203bb4971dbc08917d3b7c21e8a7928
alikats1085 <ali.frankel@gmail.com>
2022-12-18 20:00:30 -0500

    Add files via upload
    version 2 with more clickable buttons

--- script.js
+++ script.js
@@ -19,26 +19,53 @@ increaseCoffee);
 
 
 
-const dripButton=document.getElementById("buyCoffee");
-const dripCounter= document.getElementById("dripQuantity");
+// const dripButton=document.getElementById("buyCoffee");
+// const dripCounter= document.getElementById("dripQuantity");
 
-dripCounter.innerhtml =0;
-increaseDrip.onClick = ()=>{
-    dripCounter.innerHTML++;
-};
-
-
-
-// let dripCount = 0
-// const cpc= 1;
-// const increaseDrip = ()=> {
-
-//    let count = dripCount += 1;
-//     document.getElementById("buyCoffee").innerHTML= count;
-    // for (let i=0; i===15; i++){
-    //     if (document.getElementById("dripQuantity" === "click") && count >= 15
-    //      ); return coffeeCount =  i+1
-    // };
-    
+// dripCounter.innerhtml =0;
+// increaseDrip.onClick = ()=>{
+//     dripCounter.innerHTML++;
 // };
-// document.getElementById ("buyCoffee").addEventListener("click", increaseDrip)
+
+
+
+let dripCount = 0
+const cpc= 1;
+const increaseDrip = ()=> {
+
+   let count = dripCount += 1;
+    document.getElementById("buyCoffee").innerHTML= count;
+    for (let i=0; i===15; i++){
+        if (document.getElementById("dripQuantity" === "click") && count >= 15
+         ); return coffeeCount =  i+1
+    };
+    
+};
+document.getElementById ("buyCoffee").addEventListener("click", increaseDrip)
+
+let frenchCount = 0
+// const cpc= 1;
+const increaseFrench = ()=> {
+
+   let count = frenchCount += 1;
+    document.getElementById("buyFrench").innerHTML= count;
+    for (let i=0; i===15; i++){
+        if (document.getElementById("dripQuantity" === "click") && count >= 15
+         ); return coffeeCount =  i+1
+    };
+    
+};
+document.getElementById ("buyFrench").addEventListener("click", increaseFrench)
+
+let chemexCount = 0
+const increaseChemex = ()=> {
+
+   let count = chemexCount += 1;
+    document.getElementById("buyChemex").innerHTML= count;
+    for (let i=0; i===15; i++){
+        if (document.getElementById("chemexQuantity" === "click") && count >= 15
+         ); return chemexCount =  i+1
+    };
+    
+};
+document.getElementById ("buyChemex").addEventListener("click", increaseChemex)
